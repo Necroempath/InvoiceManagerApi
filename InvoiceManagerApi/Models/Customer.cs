9576a0d9c09c16aac088bc0909853cd107ace1d3
Nate <8bitsodagamer@gmail.com>
2026-01-27 16:26:21 +0400

    models created

---
+++ InvoiceManagerApi/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace InvoiceManagerApi.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public int InvoiceId { get; set; }
+    public Invoice? Invoice { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTimeOffset? UpdatedAt { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+}   
---
+++ InvoiceManagerApi/Models/Invoice.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace InvoiceManagerApi.Models;
+
+public class Invoice
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public Customer? Customer { get; set; }
+    public DateTimeOffset StartDate { get; set; }
+    public DateTimeOffset EndDate { get; set; }
+    public IEnumerable<InvoiceRow> Rows { get; set; } = [];
+    public decimal TotalSum { get; set; }
+    public string Comment { get; set; } = string.Empty;
+    public InvoiceStatus Status { get; set; } = InvoiceStatus.Created;
+    public DateTimeOffset CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTimeOffset? UpdatedAt { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+}
+
+public enum InvoiceStatus
+{
+    Created, 
+    Sent, 
+    Received, 
+    Paid, 
+    Cancelled, 
+    Rejected
+}
