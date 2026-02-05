486ce5871082bda0f77f2a34bae02930a8bcbacd
Nate <8bitsodagamer@gmail.com>
2026-02-05 14:13:50 +0400

    coomit

--- InvoiceManagerApi/obj/Debug/net9.0/InvoiceManagerApi.AssemblyInfo.cs
+++ InvoiceManagerApi/obj/Debug/net9.0/InvoiceManagerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceManagerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f9bb5acddb1ce599d80893b03259a07addc5bc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0365d463017f9fdc18eb5431a779434749afa14d")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceManagerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceManagerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
